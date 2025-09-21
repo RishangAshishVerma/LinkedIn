642def9865b1eb6333c19fdabddb783e3f0a6db6
RishangAshishVerma <vermarishang055@gmail.com>
2025-09-21 09:51:28 +0530

    Add user route and controller for fetching current user details

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 let app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 let PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 
 app.listen(PORT, () => {
