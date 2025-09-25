1832b3449d1642123475ad117595b982bd34d3a8
RishangAshishVerma <vermarishang055@gmail.com>
2025-09-26 00:30:11 +0530

    Implement connection management: add send, accept, reject, and status retrieval functionalities, and update routes accordingly

--- back-end/controllers/connection.controller.js
+++ back-end/controllers/connection.controller.js
@@ -1,7 +1,170 @@
-export const sendConnection = async(req,res) =>{
+import Connection from "../models/connection.model.js"
+import User from "../models/user.model.js"
+
+export const sendConnection = async (req, res) => {
     try {
-        
+        let { id } = req.params
+        let sender = req.userId
+        let user = await User.findById(sender)
+
+        if (sender === id) {
+            return res.status(400).json({ Message: "you can not request to yourself " })
+        }
+
+        if (user.connection.includes(id)) {
+            return res.status(400).json({ Message: "your already connected" })
+        }
+
+        let existingConnection = await Connection.findOne({
+            sender: sender,
+            receiver: id,
+            status: "pending"
+        });
+        if (existingConnection) {
+            return res.status(400).json({ Message: "request already exist" })
+        }
+
+        let newRequest = await Connection.create({
+            sender,
+            receiver: id,
+        })
+
+        return res.status(200).json(newRequest)
+
     } catch (error) {
-        
+        return res.status(500).json({ Message: `sendconnection error ${error}` })
     }
 }
+
+export const acceptConnection = async (req, res) => {
+    try {
+        let { connectionId } = req.params
+        let connection = await Connection.findById(connectionId)
+
+        if (!connection) {
+            return res.status(400).json({ message: " connection dosenot exist" })
+        }
+        if (connection.status != "pending") {
+            return res.status(400).json({ message: "request under process " })
+        }
+
+        connection.status = "accepted"
+        await connection.save()
+        await User.findByIdAndUpdate(req.userId, {
+            $addToSet: { connection: sender._id }
+        })
+        await User.findByIdAndUpdate(connection.sender._id, {
+            $addToSet: { connection: req.userId }
+        })
+
+        return res.status(200).json({ message: "connection accepted" })
+
+    } catch (error) {
+        return res.status(500).json({ message: `connection accepted error ${error}` })
+    }
+}
+
+export const rejectConnection = async (req, res) => {
+    try {
+        let { connectionId } = req.params
+        let connection = await Connection.findById(connectionId)
+
+        if (!connection) {
+            return res.status(400).json({ message: " connection dosenot exist" })
+        }
+        if (connection.status != "pending") {
+            return res.status(400).json({ message: "request under process " })
+        }
+
+        connection.status = "rejected"
+        await connection.save()
+
+
+        return res.status(200).json({ message: "connection rejected" })
+
+    } catch (error) {
+        return res.status(500).json({ message: `connection rejected error ${error}` })
+    }
+}
+
+export const getConnectionStatus = async (req, res) => {
+    try {
+        const targetUserId = req.params.userId;
+        const currentUserId = req.userId;
+
+        let currentUser = await User.findById(currentUserId)
+        if (currentUser.connection.includes(targetUserId)) {
+            return res.json({ status: "disconnect" });
+        }
+
+        const pendingRequest = await Connection.findOne({
+            $or: [
+                { sender: currentUserId, receiver: targetUserId },
+                { sender: targetUserId, receiver: currentUserId },
+            ],
+            status: "pending",
+        });
+
+        if (pendingRequest) {
+            if (pendingRequest.sender.toString() === currentUserId.toString()) {
+                return res.json({ status: "pending" });
+            } else {
+                return res.json({ status: "received", requestId: pendingRequest._id });
+            }
+        }
+
+        return res.json({ status: "connect" });
+    } catch (error) {
+        return res.status(500).json({ message: `error while getting connectiion status ${error}` })
+    }
+}
+
+export const removeConnection = async (req, res) => {
+    try {
+        const myId = req.userId;
+        const otherUserId = req.params.userId;
+
+        await User.findByIdAndUpdate(myId, { $pull: { connection: otherUserId } });
+
+        await User.findByIdAndUpdate(otherUserId, { $pull: { connection: myId } });
+
+        return res.json({ message: "connection removed successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ message: `there is error while removeing the connection ${error}` })
+    }
+}
+
+export const getConnectionRequest = async (req, res) => {
+    try {
+        const userId = req.userId
+
+        const requests = await Connection.find({
+            receiver: userId, status: "pending"
+        }).populate("sender", "firstName lastName email userName profileImage headline")
+
+        return res.status(200).json(requests)
+    } catch (error) {
+        return res.status(500).json({ message: `sever error ${error}` })
+    }
+}
+// check this code down one 
+export const getUserConnections = async (req, res) => {
+    try {
+        const userId = req.userId;  // logged-in user
+
+        // find the user and populate his connections
+        const user = await User.findById(userId)
+            .populate("connection", "firstName lastName email userName profileImage headline");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // send back the connections
+        return res.status(200).json(user.connection);
+
+    } catch (error) {
+        return res.status(500).json({ message: `error while getting user connections ${error}` });
+    }
+};
--- back-end/routes/connection.routes.js
+++ back-end/routes/connection.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { sendConnection } from "../controllers/connection.controller.js";
+import { acceptConnection, getConnectionRequest,getUserConnections, getConnectionStatus, rejectConnection, removeConnection, sendConnection } from "../controllers/connection.controller.js";
 import isAuth from "../middlewares/isAuth.middleware.js"
 
 let connectionRouter = express.Router();
 
-connectionRouter.get("/send",isAuth,sendConnection)
+connectionRouter.get("/send/:id",isAuth,sendConnection)
+connectionRouter.get("/accept/:connectionId",isAuth,acceptConnection)
+connectionRouter.get("/reject/:connectionId",isAuth,rejectConnection)
+connectionRouter.get("/getstatus/:userId",isAuth,getConnectionStatus)
+connectionRouter.get("/remove/:userId",isAuth,removeConnection)
+connectionRouter.get("/requests",isAuth,getConnectionRequest)
+connectionRouter.get("/",isAuth,getUserConnections)
 
 
 
